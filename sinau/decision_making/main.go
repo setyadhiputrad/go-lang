8d582e2902503adb86415071e1ae9c6a51ce50c5
Setyadhi-Putra-D <setyadhiputrad@gmail.com>
2019-07-21 02:01:39 +0700

    create new learning

---
+++ sinau/decision_making/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	source_if "github.com/setyadhiputrad/go-lang/sinau/decision_making/source_if"
+)
+
+func main() {
+
+	fmt.Printf("jajal if")
+	//load source
+	fmt.Println(source_if.sinauIf())
+}
---
+++ sinau/decision_making/source_if/if.go
@@ -0,0 +1,11 @@
+package source_if
+
+import (
+	"fmt"
+)
+
+func sinauIf() {
+	if true {
+		fmt.Println("testing decision making")
+	}
+}
